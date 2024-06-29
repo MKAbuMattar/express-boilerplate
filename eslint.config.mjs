5a724c09188e440f4791e1cee76919158b3def2a
MKAbuMattar <mohammad.khaled@outlook.com>
2024-06-29 15:46:49 +0300

    chore: Update npm dependency to latest stable version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,57 @@
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import js from '@eslint/js';
+import {FlatCompat} from '@eslint/eslintrc';
+
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
 /** @type {import("eslint").Linter.Config} */
-const eslintConfig = {
-  extends: [
+const eslintConfig = [
+  {
+    ignores: [
+      '**/dist',
+      '**/coverage',
+      '**/package-lock.json',
+      '**/package.json',
+    ],
+  },
+  ...compat.extends(
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-  ],
-  plugins: ['@typescript-eslint', 'simple-import-sort', 'prettier'],
-  parser: '@typescript-eslint/parser',
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
-    'prettier/prettier': 'error',
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      'simple-import-sort': simpleImportSort,
+      prettier,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'prettier/prettier': 'error',
+    },
   },
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-};
+];
 
 export default eslintConfig;
