87be6ba8c5af91ddb05780910f05edc67a116bd9
MKAbuMattar <mohammad.khaled@outlook.com>
2024-09-28 00:11:09 +0300

    refactor: Update environment configuration in env-config.util.ts

--- src/utils/env-config.util.ts
+++ src/utils/env-config.util.ts
@@ -8,14 +8,23 @@ function getEnv() {
     NODE_ENV: str({
       devDefault: testOnly('test'),
       choices: ['test', 'development', 'staging', 'production'],
+      desc: 'The environment in which the application is running',
     }),
-    HOST: host({devDefault: testOnly('localhost')}),
-    PORT: port({devDefault: testOnly(8080)}),
+    HOST: host({
+      devDefault: testOnly('localhost'),
+      desc: 'The host on which the server will run',
+    }),
+    PORT: port({
+      devDefault: testOnly(8080),
+      desc: 'The port on which the server will run',
+    }),
     CORS_WHITELIST: str({
       devDefault: testOnly('http://localhost:*'),
+      desc: 'Comma-separated list of allowed origins for CORS',
     }),
     API_KEY: str({
       devDefault: testOnly('TEST_API_KEY'),
+      desc: 'API key for authenticating requests',
     }),
   });
 }
